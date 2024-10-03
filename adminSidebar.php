1f4fbe1893199f31ae3e1bebbb013e92844d3f79
Thisara Sandapium <w.a.thisarasandapium@gmail.com>
2024-10-04 03:35:06 +0530

    updated

--- adminSidebar.php
+++ adminSidebar.php
@@ -1,38 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style\adminPanel.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-    />
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style\adminPanel.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+  <title>Document</title>
 </head>
+
 <body>
-<div class="sidebar">
-      <h2>Admin Panel</h2>
-      <ul>
-        <li>
-          <a href="adminUsers.php"><i class="fas fa-users"></i> Users</a>
-        </li>
-        <li>
-          <a href="#"><i class="fas fa-car"></i> Vehicle</a>
-        </li>
-        <li>
-          <a href="#"><i class="fas fa-calendar-check"></i> Reservation</a>
-        </li>
-        <li>
-          <a href="csrRegistration.php"><i class="fas fa-briefcase"></i> Employees</a>
-        </li>
-        <li>
-          <a href="#"><i class="fas fa-credit-card"></i> Payment</a>
-        </li>
-        <li>
-          <a href="adminPackage.php"><i class="fas fa-cogs"></i> package</a>
-        </li>
-      </ul>
-    </div>
+  <div class="sidebar">
+    <h2>Admin Panel</h2>
+    <ul>
+      <li>
+        <a href="adminPanel.php"><i class="fas fa-home"></i> Admin Home</a>
+      </li>
+      <li>
+        <a href="adminUsers.php"><i class="fas fa-users"></i> Users</a>
+      </li>
+      <li>
+        <a href="#"><i class="fas fa-car"></i> Vehicle</a>
+      </li>
+      <li>
+        <a href="#"><i class="fas fa-calendar-check"></i> Reservation</a>
+      </li>
+      <li>
+        <a href="csrRegistration.php"><i class="fas fa-briefcase"></i> Employees</a>
+      </li>
+      <li>
+        <a href="#"><i class="fas fa-credit-card"></i> Payment</a>
+      </li>
+      <li>
+        <a href="adminPackage.php"><i class="fas fa-cogs"></i> package</a>
+      </li>
+    </ul>
+  </div>
 </body>
+
 </html>
